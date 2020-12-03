0312374513aaa6afe685f4f3a4dd607d8daa2886
Michael McGarty <Mcgartym01@yahoo.com>
2020-12-02 22:37:13 -0500

    Add files via upload
    version 1 completion

--- remask.py
+++ remask.py
@@ -4,8 +4,9 @@ import cv2 # Computer vision library
 import moviepy.editor # Video functions (length, etc.)
 import os # (Deleting files)
 from os import path # (Filepath helper)
-import numpy as np # ???
-import sys # ???
+import numpy as np
+import sys
+import math
 
 # Global variable to define the fps of the video file to be used
 fps = 30
@@ -13,14 +14,18 @@ fps = 30
 # Defines the number of seconds until RGB values are captured.
 TIME_OFFSET = 30 
 
-# Define points to be used to determine baseline color. See README for a diagram and more info.
-baseline = [(43, 208), (60, 249), (90, 281), (134, 300), (211, 301), (255, 281), (282, 249), (298, 207)]
-
 # Define points to be sampled in the center circle of the dosimeter for testing UV exposure levels.
-testing = [(147, 110), (192, 110), (227, 148), (227, 190), (192, 227), (147, 227), (115, 190), (115, 148)]
+# testing = [(147, 110), (192, 110), (227, 148), (227, 190), (192, 227), (147, 227), (115, 190), (115, 148)]
+testing = []
+baseline = []
+base_rgb = []
 
-video_name = ""
+# Define the size ratio of the concentric circles on the dosimeter.
+inner_ratio = 0.56
+outer_ratio = 0.44
 
+vid_name = ""
+video_length = 0
 # Defines the number of samples which will be done when searching for the dosimeter.
 NUM_SAMPLES = 5
 
@@ -29,21 +34,23 @@ X_OFF = 10
 Y_OFF = 10
 
 # RGB intervals used to filter the dosimeter from the image.
-R_LOWER = 140
+R_LOWER = 55
 R_UPPER = 200
-G_LOWER = 70
+G_LOWER = 40
 G_UPPER = 185
-B_LOWER = 40
-B_UPPER = 155
+B_LOWER = 25
+B_UPPER = 250
 
 # Defines the pixel tolerance when determining the dosimeter size.
 # Once the dosimeter size is determined, the tolerance prevents random issues from losing the dosimeter.
-TOLERANCE = 15
+TOLERANCE = 25
 
 # Default values for the expected dosimeter size. If the dosimeter is not detected, modify these values.
 DOS_SIZE_SET = False
-DOS_MIN_SIZE = 300
-DOS_MAX_SIZE = 400
+DOS_MIN_SIZE = 100
+DOS_MAX_SIZE = 500
+DOS_WIDTH = 0
+DOS_HEIGHT = 0
 
 # Boolean function which determines whether or not the dosimeter is enclosed in a region.
 # Treating the center of the region of interest, ROI, as the the origin, The X and Y offsets
@@ -92,7 +99,7 @@ def in_size(ROI):
     return False
 
 # Image processing function which crops each individual frame into purely the dosimeter.
-def processFrames(frameName, m, s, c):
+def processFrames(frameName, write):
     # Read the frame into memory.
     frame = cv2.imread(frameName, cv2.IMREAD_COLOR)
 
@@ -100,7 +107,7 @@ def processFrames(frameName, m, s, c):
     if (frame is None):
         print("Invalid file path for sample image.")
         # TO DO: Potentially add error handling.
-        return
+        return False
 
     # Convert the original image into grayscale, and apply OTSU filtering to it.
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -114,7 +121,6 @@ def processFrames(frameName, m, s, c):
 
     # Loop through all the contours to find the dosimeter.
     found_contour = False
-    global DOS_SIZE_SET, DOS_MIN_SIZE, DOS_MAX_SIZE
     
     for c in contours:
         # Extract the region enclosing a specific contour.
@@ -124,30 +130,32 @@ def processFrames(frameName, m, s, c):
         if in_size(ROI) and isDosimeter(ROI):
             found_contour = True
 
+            global DOS_SIZE_SET, DOS_MIN_SIZE, DOS_MAX_SIZE, DOS_HEIGHT, DOS_WIDTH
             if DOS_SIZE_SET == False:
+                print("Set the dosimeter!")
                 width, height,_ = ROI.shape
                 DOS_MIN_SIZE = min(width, height) - TOLERANCE
                 DOS_MAX_SIZE = max(width, height) + TOLERANCE
                 DOS_SIZE_SET = True
-                #cv2.imwrite("Origin_cropped.jpg", ROI)
+                cv2.imwrite("Origin_cropped.jpg", ROI)
+            
+            if DOS_WIDTH == 0 and DOS_HEIGHT == 0:
+                DOS_WIDTH, DOS_HEIGHT,_ = ROI.shape
+
             break
 
     # Terminate program if contour is not found.
     # Feel free to comment out to prevent terminal spam.
-    if not found_contour:
-        print("ERROR: Contour was not found in frame_{}_{}_{}.jpg".format(m,s,c))
-        print("Consider modifying of RBG constants or Dosimeter size constants at top of file.")
-        print("Current frame will be skipped.\n")
-        # sys.exit(0)
+    if found_contour == False:
+        print("ERROR: Contour was not found in {}".format(frameName))
+        # print("Consider modifying of RBG constants or Dosimeter size constants at top of file.")
+        # print("Current frame will be skipped.\n")
+        return False
 
-    try:
-        os.remove("frameName")
-    except:
-        pass
-
-    processed_file_name = "frame_{}_{}_{}.jpg".format(m, s, c)
-    cv2.imwrite(processed_file_name, ROI)
-    return
+    if (write == True):
+        cv2.imwrite(frameName, ROI)
+    
+    return True
 
 # Dosimeter size parameters initialization function.
 # Depending on how the video is shot and where the dosimeter is within it, the number of pixels 
@@ -175,14 +183,59 @@ def initDosSize(filename):
     success,origin_frame = video.read()
     cv2.imwrite("origin_frame.jpg", origin_frame)
 
-    processFrames("origin_frame.jpg", "dosimiter","cailbration","frame")
+    processFrames("origin_frame.jpg", True)
 
-    # Debugging to verify dosimeter is found.
-    # try:
-    #     os.remove("Origin_frame.jpg")
-    #     os.remove("Origin_cropped.jpg")
-    # except:
-    #     pass
+    if (DOS_SIZE_SET == True):
+        return True
+
+    return False
+
+def initSamples():
+    # Assumes the sizes are valid, since the initDosSize function will cause program to terminate
+    # if the dosimeter was not found prior.
+    
+    # Testing is a list of tuples corresponding to all the points RGB values will be sampled.
+    global testing, baseline
+    tolerance = math.floor((DOS_WIDTH + DOS_HEIGHT) / 20) # Average, w and h and divide by 10.
+    w_center = DOS_WIDTH // 2
+    h_center = DOS_HEIGHT // 2
+
+    inner_diam = inner_ratio * (DOS_WIDTH + DOS_HEIGHT) // 4
+    outer_width = outer_ratio * (DOS_WIDTH + DOS_HEIGHT) // 8
+    outer_offset = DOS_WIDTH - outer_width
+
+    testing_x_pos =  [w_center, w_center, w_center + inner_diam - tolerance, w_center, w_center - inner_diam + tolerance]
+    testing_y_pos = [h_center, h_center - inner_diam + tolerance, h_center, h_center + inner_diam - tolerance, h_center]
+
+    baseline_x_pos = [outer_width, outer_width + tolerance, outer_width + (2 * tolerance), outer_width + (3 * tolerance), 
+    outer_offset, outer_offset - tolerance, outer_offset - (2 * tolerance), outer_offset - (3 * tolerance)]
+    
+    baseline_y_pos = [h_center + tolerance, h_center + (2 * tolerance), h_center + (3 * tolerance), h_center + (4 * tolerance),
+    h_center + tolerance, h_center + (2 * tolerance), h_center + (3 * tolerance), h_center + (4 * tolerance)]
+
+    # List includes the sample points for both the inner circle and outer ring.
+    # x_pos =  [w_center, w_center, w_center + inner_diam - tolerance, w_center, w_center - inner_diam + tolerance,
+    # outer_width, outer_width + tolerance, outer_width + (2 * tolerance), outer_width + (3 * tolerance), 
+    # outer_offset, outer_offset - tolerance, outer_offset - (2 * tolerance), outer_offset - (3 * tolerance)]
+    
+    # y_pos = [h_center, h_center - inner_diam + tolerance, h_center, h_center + inner_diam - tolerance, h_center,
+    # h_center + tolerance, h_center + (2 * tolerance), h_center + (3 * tolerance), h_center + (4 * tolerance),
+    # h_center + tolerance, h_center + (2 * tolerance), h_center + (3 * tolerance), h_center + (4 * tolerance)]
+
+    test_samples = len(testing_y_pos)
+    baseline_samples = len(baseline_x_pos)
+
+    print("(X,Y) points which will sample the dosimeter.")
+    for i in range(test_samples):
+        testing.append((testing_x_pos[i], testing_y_pos[i]))
+        print(testing[i])
+
+    print()
+    
+    print ("(X, Y) points which will sample the baseline value of the dosimeter")
+    for i in range(baseline_samples):
+        baseline.append((baseline_x_pos[i], baseline_y_pos[i]))
+        print(baseline[i])
     return
 
 def getPixelColor(image_name, x, y):
@@ -196,55 +249,15 @@ def getPixelColor(image_name, x, y):
     image_rgb = image.convert("RGB")
 
     # Get color from (x, y) coordinates
+    w = int(x)
+    h = int(y)
+    if (w < 0 or w >= DOS_WIDTH):
+        return -1, -1, -1
+    if (h < 0 or h >= DOS_HEIGHT):
+        return -1, -1, -1
+
     r, g, b = image_rgb.getpixel((int(x),int(y)))
-
-    return r, g, b
-
-# TODO: Figure out how to standardize the folder path so that it's not hardcoded here...
-# Should be able to create a folder above and 
-def deleteFrames():
-    #providing the path of the folder
-    #r = raw string literal
-    folder_path = ('/Users/tylerharkcom/Desktop/rgb')
-    #using listdir() method to list the files of the folder
-    test = os.listdir(folder_path)
-    #taking a loop to remove all the images
-    #using ".png" extension to remove only png images
-    #using os.remove() method to remove the files
-    for images in test:
-        if images.endswith(".jpg"):
-            os.remove(os.path.join(folder_path, images))
-
-def getData(video_length, filter, filename):
-    minute = 0
-    second = 0
-    count = 1
-    minutes, seconds = convertTime(video_length)
-    data_file = filename + ".csv"
-    with open(data_file, 'w') as csvfile:
-        filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        r_b, g_b, b_b = sampleColor("frame_0_0_1.jpg", baseline)
-        filewriter.writerow(['Baseline', 'RGB', 'Color', r_b, g_b, b_b])
-        filewriter.writerow(['Minute', 'Second', 'Frame', 'R', 'G', 'B'])
-        if filter:
-            for minute in range(0, (minutes + 1)):
-                for second in range(0, 60, 10):
-                    for count in range (1, 31):
-                        frame = "frame_%d_%d_%d.jpg" % (minute, second, count)
-                        if path.exists(frame) != True:
-                            break
-                        r, g, b = sampleColor(frame, testing)
-                        filewriter.writerow([minute, second, count, r, g, b])
-        else:
-            for minute in range(0, (minutes + 1)):
-                for second in range(0, 60):
-                    for count in range (1, 31):
-                        frame = "frame_%d_%d_%d.jpg" % (minute, second, count)
-                        if path.exists(frame) != True:
-                            break
-                        r, g, b = sampleColor(frame, testing)
-                        filewriter.writerow([minute, second, count, r, g, b])
-    deleteFrames()
+    return 
 
 def convertTime(secs):
     mins = secs // 60
@@ -259,8 +272,9 @@ def sampleColor(image_name, values):
     try:
         image = Image.open(image_name)
     except IOError:
-        # print("Error in sampleColor(): Image not found.")
+        print("Error in sampleColor(): Image: {} was not found.".format(image_name))
         return None
+
     # Convert to RGB colorspace
     image_rgb = image.convert("RGB")
 
@@ -268,12 +282,14 @@ def sampleColor(image_name, values):
     green = []
     blue = []
 
+    # Loop over the predetermined sample points and append rgb values to list.
     for x, y in values:
         r, g, b = image_rgb.getpixel((int(x),int(y)))
         red.append(r)
         green.append(g)
         blue.append(b)
 
+    # Average the rgb values for each sample point.
     red_val = round(average(red))
     green_val = round(average(green))
     blue_val = round(average(blue))
@@ -283,7 +299,7 @@ def sampleColor(image_name, values):
 # Function to extract frames
 # "filename" is a string that contains the name of the video file to be processed
 # "filter" is a boolean that denotes whether to filter frames or not
-# When filter = 1, only the 30 frames from each 10th second are captured (30 frames @ 0, 10, 20 30, etc.)
+# When filter = 1, only the frames from each 10th second are captured (Ex 30 frames @ 0, 10, 20 30, etc.)
 # When filter = 0, all frames are captured
 def getTheFrames(filename, filter):
     # Path to video file
@@ -294,40 +310,87 @@ def getTheFrames(filename, filter):
     second = 0
     minute = 0
     framecount = 0
+    frame_offset= fps* TIME_OFFSET
 
-    # checks whether frames were extracted
+    # success fiend shows whether a frame was correctly extracted
     success = 1
 
-    while success:
-        # vidObj object calls read
-        # function extract frames
-        success, image = video.read()
+    # Create a local directory storing all of the frames for a given test.
+    try:
+        os.mkdir(vid_name + "_results")
+    except:
+        pass # If the file exists, its fine.
 
-        # Set bounds on frames between 1 and 30, wrapping back to 1
-        if (count > 30):
-            second += 1
-            count = 1
+    # Change to the created directory.
+    os.chdir(vid_name + "_results")
+    
+    # Open the csv file.
+    data_file = vid_name + ".csv"
 
-        # Set bounds on seconds from 0 to 59, wrapping back to 0
-        # Ex: 0:59:28...0:59:29...0:59:30...1:00:01...1:00:02...
-        if (second == 60):
-            minute += 1
-            second = 0
+    # Create/open the csv file for writing.
+    with open(data_file, "w") as csvfile:
+        file_writer = csv.writer(csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        file_writer.writerow(["Baseline RGB values:", "R", "G", "B"])
+        file_writer.writerow(["", base_rgb[0], base_rgb[1], base_rgb[2]])
+        file_writer.writerow(["Minute", "Second", "Frame", "R", "G", "B"])
+        
+        while success:
+            # vidObj object calls read function to extract a singular frame
+            success, image = video.read()
 
-        if filter:
-            if second % 10 == 0:
-                frame = ("frame_%d_%d_%d.jpg" % (minute, second, count))
-                cv2.imwrite(frame, image)
-                processFrames(frame, minute, second, count)
+            # Set bounds on frames between 1 and 30, wrapping back to 1
+            if (count > fps):
+                second += 1
+                count = 1
+
+            # Set bounds on seconds from 0 to 59, wrapping back to 0
+            # Ex: 0:59:28...0:59:29...0:59:30...1:00:01...1:00:02...
+            if (second == 60):
+                minute += 1
+                print("{} minute(s) processed.".format(minute))
+                second = 0
+
+            # Skip the first thirty seconds of video.
+            if (framecount < frame_offset):
                 framecount += 1
-        else:
+                count += 1
+                continue
+
             frame = ("frame_%d_%d_%d.jpg" % (minute, second, count))
-            cv2.imwrite(frame, image)
-            processFrames(frame, minute, second, count)
-            framecount += 1
+            if filter:
+                if second % 10 == 0:
+                    cv2.imwrite(frame, image)
+                    framecount += 1
 
-        count += 1
+                    if (processFrames(frame, True) == False):
+                        file_writer.writerow([minute, second, count, -1, -1, -1])
+                        continue
 
+                    r, g, b = sampleColor(frame,testing)
+                    file_writer.writerow([minute, second, count, r, g, b])
+                    
+            else:
+                cv2.imwrite(frame, image)
+                framecount += 1
+
+                if (processFrames(frame, True) == False):
+                        file_writer.writerow([minute, second, count, -1, -1, -1])
+                        continue
+
+                r, g, b = sampleColor(frame,testing)
+                file_writer.writerow([minute, second, count, r, g, b])
+
+            count += 1
+
+            # Uncomment to remove the cropped images from being saved.
+            # try:
+            #     os.remove(str(frame))
+            # except:
+            #     pass
+            
+
+    # return to folder above.
+    os.chdir("../")
     return framecount
 
 def main():
@@ -344,8 +407,9 @@ def main():
     # rsplit() returns an array, split on the delimiter passed in
     # Here, it splits the ".mp4" off of the video file
     # (Ex: "test.123.mp4" becomes ["test.123", "mp4"])
-    vid_name = filename.rsplit('.',1)
-
+    global vid_name, video_length
+    vid_name = filename.rsplit('.',1)[0]
+    
     # Get video length in minutes/seconds for frame iteration purposes
     video = moviepy.editor.VideoFileClip(filename)
     video_length = int(video.duration)
@@ -366,21 +430,26 @@ def main():
         choice = int(input())
 
     # Initialize the dosimeter size for the given video.
-    initDosSize(filename)
+    if (initDosSize(filename) == False):
+        print("Was unable to determine dosimeter from provided video. Please modify the rgb or size thresholds based on the origin image")
+        print("Terminating the program...")
+        sys.exit(1)
+
+    # Initialize the sample points based off the size of the dosimeter.
+    initSamples()
+
+    # Calculate the baseline rgb values for the origin frame.
+    global base_rgb
+    base_r, base_g, base_b = sampleColor("Origin_cropped.jpg", baseline)
+    base_rgb = [base_r, base_g, base_b]
+
+    print(base_rgb)
 
     # Call getTheFrames to parse the video, called "filename", into individual frames
     print("Fetching frames. This may take some time, please wait...")
+
     count = getTheFrames(filename, choice)
     print("Successfully exported %d frames." % count)
-
-    print("Processing RGB values for each frame. This may take some time, please wait...")
-
-    # Get rgb data for the selected x,y coordinate over the generated frames
-    # Deletes all frame files once the data is captured
-    # vid_name[0] passes the full name of the video file without the ".mp4"
-    # (Ex: "test.123.mp4" becomes "test.123")
-    getData(video_length, choice, vid_name[0])
-
 
 if __name__ == "__main__":
     main()
